61fe74f3e937d37f1fa1fa52fe7585a8d1d995db
pooya7 <79900252+pooya7@users.noreply.github.com>
2021-03-07 21:55:55 +0330

    and that is that

---
+++ djangoBlog/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path , include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+from tamrin.views import articles_list
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/' , views.about),
+    path('tamrin/' , include('tamrin.urls')),
+    path('accounts/',include('accounts.urls')),
+    path('', articles_list),
+
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
